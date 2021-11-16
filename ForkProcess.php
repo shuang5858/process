fb4c7b3676b975b9d14678eba001c866f5c021b2
李爽 <lishuang@lishuangdeMacBook-Pro.local>
2021-11-16 17:13:30 +0800

    .

--- ForkProcess.php
+++ ForkProcess.php
@@ -1,39 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lishuang
- * Date: 11/5/21
- * Time: 5:14 PM
- */
+
+pcntl_async_signals(true);
 
 class ForkProcess
 {
     public static $aConfig = array(
-        'daemonize'            => false,
+        'daemonize'            => true,
         'master_process_title' => 'master:process-pool',
         'worker_process_title' => 'worker:process-pool',
         'master_pid_file'      => "master.pid",
         'child_pid_file'       => "child.pid",
     );
 
-    public function __construct()
-    {
-        $aConfigFile = parse_ini_file('./config.ini');
-
-        self::$aConfig = array_merge(self::$aConfig, $aConfigFile );
-        pcntl_async_signals(true);
-    }
     public static $aChildPids = array();
     public static $sRoot      = "";
     public static $iMasterPid = 0;
 
+    public function __construct()
+    {
+        $aConfigFile = parse_ini_file('./config.ini');
+
+        self::$aConfig = array_merge(self::$aConfig, $aConfigFile);
+
+        self::$sRoot = __DIR__;
+    }
+
     public function start()
     {
+        $bDaemon = self::$aConfig['daemonize'];
+        if (1 == $bDaemon || true == $bDaemon) {
+            $iDaemonPid =  $this->initDaemon();
+            file_put_contents(self::$aConfig['log_path'], 'iDaemonPid'.$iDaemonPid.PHP_EOL, FILE_APPEND);
+        }
 
-        $this->initDaemon();
         $this->initSignalHandler();
-        $this->getForkProcess(self::$aConfig['process_num']);
+        $this->ForkProcess(self::$aConfig['process_num']);
         $this->initMaster();
+
+        if (1 == $bDaemon || true == $bDaemon) {
+            $this->flushData2FileAfterAllOver();
+        }
 
         while(true){
             sleep(1);
@@ -41,7 +47,7 @@ class ForkProcess
     }
 
     /** 创建子进程 */
-    public function getForkProcess($iNum)
+    public function ForkProcess($iNum)
     {
         for($i = 1; $i <= $iNum; $i++) {
             $iPid = pcntl_fork();
@@ -92,12 +98,18 @@ class ForkProcess
                 file_put_contents(self::$aConfig['log_path'], '回收进程'.PHP_EOL, FILE_APPEND);
                 $this->reForkProcess();
                 break;
+
+            case SIGTERM: //daemon终止前需要做一些清理工作 init发完SIGTERM 5秒后会发送一个SIGKILL信号
+                $this->stopAll();
+                break;
+
         }
     }
 
     public function initSignalHandler()
     {
         pcntl_signal(SIGCHLD, array($this, 'signalHandler'), true);
+        pcntl_signal(SIGTERM, array($this, 'signalHandler'), true);
     }
 
     public function initDaemon()
@@ -131,6 +143,7 @@ class ForkProcess
         return $iPid;
 
     }
+
     public function initMaster()
     {
         $iMasterPid = posix_getpid();
@@ -139,4 +152,59 @@ class ForkProcess
 
     }
 
+    public function getStatus()
+    {
+        $sChildPidFile  = self::$sRoot.DIRECTORY_SEPARATOR.self::$aConfig['child_pid_file'];
+        $sChildPidFileDir  = dirname($sChildPidFile);
+        if (!is_dir($sChildPidFileDir)) {
+            mkdir($sChildPidFileDir, 0777, true);
+        }
+        file_put_contents($sChildPidFile, json_encode(self::$aChildPids));
+        print_r(self::$iMasterPid);
+        print_r(self::$aChildPids);
+    }
+
+    public function stop()
+    {
+        $sConfigFile = './config.ini';
+        if (is_file($sConfigFile)) {
+            $aConfigFromFile = parse_ini_file("./config.ini");
+        }
+        self::$aConfig = array_merge(self::$aConfig, $aConfigFromFile);
+        $sMasterPidFile = __DIR__.DIRECTORY_SEPARATOR.self::$aConfig['master_pid_file'];
+        $iMasterPid = file_get_contents($sMasterPidFile);
+        posix_kill($iMasterPid, SIGTERM);
+    }
+
+    public function stopAll()
+    {
+        $sMasterPidFile = self::$sRoot.DIRECTORY_SEPARATOR.self::$aConfig['master_pid_file'];
+        $sChildPidFile  = self::$sRoot.DIRECTORY_SEPARATOR.self::$aConfig['child_pid_file'];
+        @unlink($sMasterPidFile);
+        @unlink($sChildPidFile);
+        $aChildPids = self::$aChildPids;
+        $iMasterPid = self::$iMasterPid;
+        posix_kill($iMasterPid, SIGKILL);
+        sleep(1);
+        while (count($aChildPids) > 0) {
+            foreach($aChildPids as $iChildPid) {
+                posix_kill($iChildPid, SIGKILL);
+            }
+        }
+    }
+
+    public function flushData2FileAfterAllOver()
+    {
+        $sMasterPidFile = self::$sRoot.DIRECTORY_SEPARATOR.self::$aConfig['master_pid_file'];
+        $sChildPidFile  = self::$sRoot.DIRECTORY_SEPARATOR.self::$aConfig['child_pid_file'];
+        $sMasterPidFileDir = dirname($sMasterPidFile);
+        $sChildPidFileDir  = dirname($sChildPidFile);
+        if (!is_dir($sMasterPidFileDir)) {
+            mkdir($sMasterPidFileDir, 0777, true);
+        }
+        if (!is_dir($sChildPidFileDir)) {
+            mkdir($sChildPidFileDir, 0777, true);
+        }
+        file_put_contents($sMasterPidFile, self::$iMasterPid);
+    }
 }
